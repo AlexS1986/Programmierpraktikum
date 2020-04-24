a84414994a48d8fe93bf289618c6b8195110cfad
AlexS1986 <alexander.schlueter86@gmx.de>
2020-04-24 11:25:29 +0200

    Main component

---
+++ ProPra2020_workspace/Main_Component/src/fernuni/propra/main/GeneralException.java
@@ -0,0 +1,21 @@
+package fernuni.propra.main;
+
+public class GeneralException extends Exception {
+
+	public GeneralException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GeneralException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GeneralException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
---
+++ ProPra2020_workspace/Main_Component/src/fernuni/propra/main/ParameterSetException.java
@@ -0,0 +1,19 @@
+package fernuni.propra.main;
+
+public class ParameterSetException extends Exception {
+
+	public ParameterSetException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParameterSetException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParameterSetException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
